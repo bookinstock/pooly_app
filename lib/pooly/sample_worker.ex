d0c9a3f20ac9a4ba76e439006dd0f6ba33015196
Wende Lu <wendelu@wende-mac.local>
2019-08-14 08:00:44 +0800

    init

---
+++ lib/pooly/sample_worker.ex
@@ -0,0 +1,23 @@
+defmodule SampleWorker do
+  use GenServer
+
+  def start_link(_) do
+  	GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def stop(pid) do
+  	GenServer.call(pid, :stop)
+  end
+
+  def state(pid) do
+  	GenServer.call(pid, :state)
+  end
+
+  def handle_call(:stop, _from, state) do
+  	{:stop, :normal, :ok, state}
+  end
+
+  def handle_call(:state, _from, state) do
+  	{:reply, state, state}
+  end
+end
---
+++ lib/pooly/server.ex
@@ -0,0 +1,32 @@
+defmodule Pooly.Server do
+	use GenServer
+	import Supervisor.Spec
+
+	defmodule State do
+		defstruct sup: nil, size: nil, mfa: nil
+	end
+
+	def start_link(sup, pool_config) do
+		GenServer.start_link(__MODULE__, [sup, pool_config], name: __MODULE__)
+	end
+
+	def init([sup, pool_config]) when is_pid(pid) do
+		init(pool_config, %State{sup: sup})
+	end
+
+	def init([{:size, size} | rest], state) do
+		init(rest, %{state | size: size})
+	end
+
+	def init([{:mfa, mfa} | rest], state) do
+		init(rest, %{state | mfa: mfa})
+	end
+
+	def init([_ | rest], state) do
+		init(rest, state)
+	end
+
+	def init([], state) do
+		{:ok, state}
+	end
+end
